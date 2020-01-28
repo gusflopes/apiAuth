4298f93f126ea81e0ae5e41d0df44fb0866b7f55
gusflopes <gusflopes86@gmail.com>
2020-01-28 14:11:20 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParsers = require("body-parser");
+
+const app = express();
+
+// Middlewares
+
+// Routes
+
+// Start the server
