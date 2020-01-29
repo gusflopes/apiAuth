f416c6e1e6ce7cb75c1c9cc4c445bc1afaeed05b
gusflopes <gusflopes86@gmail.com>
2020-01-29 06:51:13 -0400

    feat: Pages and Components structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
 import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './pages/Home'
 
-export default () => {
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
+
+export default (props) => {
   return (
     <BrowserRouter>
-        <Header />
-        <p>Is this working?</p>
-        <p>https://www.youtube.com/watch?v=BerEJPQOwOE&list=PLSpJkDDmpFZ7GowbJE-mvX09zY9zfYatI&index=14</p>
+    <Header />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/signup" component={SignUp} />
+    <Route exact path="/signin" component={SignIn} />
+    <Route exact path="/dashboard" component={Dashboard} />
+    {props.children}
     </BrowserRouter>
   )
 };
---
+++ frontend/src/components/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function components() {
+  return (
+    <div>This is a SignIn Components</div>
+  );
+}
